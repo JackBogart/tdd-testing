92a59eaa9fdb84d22b1d9b6d9aa8385f89902a22
Jack Bogart <jackbogart0822@gmail.com>
2024-08-20 01:54:30 -0400

    Add reverseString function and unit tests

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 export function capitalize(input) {
   return input.charAt(0).toUpperCase() + input.slice(1);
 }
+
+export function reverseString(input) {
+  return input.split('').reverse().join('');
+}
--- scripts.test.js
+++ scripts.test.js
@@ -1,4 +1,4 @@
-import { capitalize } from './scripts';
+import { capitalize, reverseString } from './scripts';
 
 test('Capitalizes lowercase word', () => {
   const input = 'hello world';
@@ -16,4 +16,22 @@ test('Capitalizes uppercase word 2', () => {
   const input = 'Bill cipher';
 
   expect(capitalize(input)).toBe('Bill cipher');
+});
+
+test('Reverses palindrome', () => {
+  const input = 'racecar';
+
+  expect(reverseString(input)).toBe('racecar');
+});
+
+test('Reverses palindrome 2', () => {
+  const input = 'aba';
+
+  expect(reverseString(input)).toBe('aba');
+});
+
+test('Reverses string', () => {
+  const input = 'Gravity Falls';
+
+  expect(reverseString(input)).toBe('sllaF ytivarG');
 });
